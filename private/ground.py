baaab483c62f70038fcdfd405a7a36f25346b7d9
psi0000 <qkrwk0921@naver.com>
2025-06-08 09:18:00 +0000

    add private directory

---
+++ private/ground.py
@@ -0,0 +1,23 @@
+from groundingdino.util.inference import load_model, load_image, predict, annotate
+import cv2
+
+model = load_model("../groundingdino/config/GroundingDINO_SwinT_OGC.py", "../weights/groundingdino_swint_ogc.pth")
+IMAGE_PATH = "image.png"
+TEXT_PROMPT = "top region of Fire extinguisher . person . door . Hanging objects ."
+BOX_TRESHOLD = 0.35
+TEXT_TRESHOLD = 0.25
+
+image_source, image = load_image(IMAGE_PATH)
+
+boxes, logits, phrases = predict(
+    model=model,
+    image=image,
+    caption=TEXT_PROMPT,
+    box_threshold=BOX_TRESHOLD,
+    text_threshold=TEXT_TRESHOLD
+)
+
+annotated_frame = annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
+print("Boxes:", boxes)
+print("Boxes.shape:", boxes.shape)
+cv2.imwrite("annotated_image.jpg", annotated_frame)
